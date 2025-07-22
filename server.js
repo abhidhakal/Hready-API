27b90533a39b5ef08b897af5a06f31cf871aaba1
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2025-07-22 18:36:18 +0545

    port updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const app = require('./index');
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const app = require("./index");
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running");
 });
