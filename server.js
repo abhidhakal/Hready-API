54f74488e07e56e4cb051dd71bcfdcfda2a049bd
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2025-07-22 18:28:19 +0545

    cors config

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
-const app = require("./index");
-
+const app = require('./index');
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
