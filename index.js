0e1898d6847895128c47373aca3e3f6771ba8f05
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2025-07-22 18:30:52 +0545

    cors updated

--- index.js
+++ index.js
@@ -29,12 +29,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(logger);
 
